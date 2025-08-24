cbcc973f5664b81ef56d286a63b027b9cbd714f7
Steven Yates <stevenyates0705@gmail.com>
2025-08-24 16:04:19 -0500

    Pokemon FRLG: Fix Reading Remote Item Bit in Client

--- worlds/pokemon_frlg/client.py
+++ worlds/pokemon_frlg/client.py
@@ -307,7 +307,7 @@ class PokemonFRLGClient(BizHawkClient):
                 return False
 
             options_address = data.rom_addresses["gArchipelagoOptions"][self.game_version]
-            remote_items_bytes = (await bizhawk.read(ctx.bizhawk_ctx, [(options_address + 0x4D, 1, "ROM")]))[0]
+            remote_items_bytes = (await bizhawk.read(ctx.bizhawk_ctx, [(options_address + 0x4F, 1, "ROM")]))[0]
             remote_items = int.from_bytes(remote_items_bytes, "little")
         except UnicodeDecodeError:
             return False
